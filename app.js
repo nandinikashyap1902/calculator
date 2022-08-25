097a17fe3c7ae7ede2c938df319e3f4c7afd723a
nandini <nandinikashyap1902@gmail.com>
2022-08-25 11:59:41 +0530

    calculator

---
+++ app.js
@@ -0,0 +1,14 @@
+(function(){
+    let screen = document.querySelector('.screen');
+    let buttons = document.querySelector('.btn');
+    let clear = document.querySelector('.btn-clear');
+    let equal = document.querySelector('.btn-equal');
+
+buttons.forEach(function(button){
+    button.addEventListener('click',function(e){
+        let value = e.target.dataset.num;
+        screen.value +=value;
+    })
+});
+
+})(); 
